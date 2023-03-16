deab0dce3269eb650d021983d434170e332cf3c3
Antonio Aguilera <antonio.aguilera@adevinta.com>
2023-03-16 10:13:30 +0100

    first commit

---
+++ src/main/kotlin/tony/aguilera/kotlinddd/KotlinDddApplication.kt
@@ -0,0 +1,11 @@
+package tony.aguilera.kotlinddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDddApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDddApplication>(*args)
+}
